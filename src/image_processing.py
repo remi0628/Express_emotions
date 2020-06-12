e253971071eee974cf4d7f79302091eb9a98da82
remi0628 <safaia0628@yahoo.co.jp>
2020-06-12 21:56:23 +0900

    ituki file

--- src/image_processing.py
+++ src/image_processing.py
@@ -5,21 +5,22 @@ import math
 
 def main():
 	# 画像の読み込み
-	src_img = cv2.imread('../img/happy.png', -1)
+	src_img = cv2.imread('../img/happy.png')
 	#back_img = cv2.imread('../img/green_back.jpg')
 	# 画像のリサイズ
-	src_img = image_resize(src_img)
+	img = image_resize(src_img)
+	'''
+	### 読み込んだ画像に背景を付ける ###
 	# 画像に合わせたNumPy配列を記録
-	h, w, c = src_img.shape
+	height, width, channel = img.shape
 	# 変換行列生成
-	mat = cv2.getRotationMatrix2D((w / 2, h / 2), 0, 0.5)
+	mat = cv2.getRotationMatrix2D((width / 2, height / 2), 0, 0.5)
 	# アフィン変換
-	img = cv2.warpAffine(src_img, mat, (500, 500), borderValue=(0, 128, 255)) # アフィン変換
+	img = cv2.warpAffine(img, mat, (500, 500), borderValue=(0, 128, 255)) # アフィン変換
+	'''
 	            
 
 	cv2.imshow('image', img)
-
-
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
