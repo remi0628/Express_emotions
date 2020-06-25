1d4301279a4c01b8c811ae47d74dec6883286b9c
Itsuki Okada <m011906297@edu.teu.ac.jp>
2020-06-25 10:32:49 +0900

    test10

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
 import math
-
-
+ 
 
 def main():
 	# 画像の読み込み
@@ -10,6 +9,7 @@ def main():
 	#back_img = cv2.imread('../img/green_back.jpg')
 	# 画像のリサイズ
 	img = image_resize(src_img)
+
 	'''
 	### 読み込んだ画像に背景を付ける ###
 	# 画像に合わせたNumPy配列を記録
@@ -27,11 +27,23 @@ def main():
 
 
 # 画像リサイズ
+'''
 def image_resize(image):
     img = image
     height, width = img.shape[:2]
-    if 500 <= height or 450 <= width:
+    
+    if 500 <= height or 500 <= width:
         img = cv2.resize(img , (int(width*0.5), int(height*0.5)))
+    return img
+
+'''
+
+def image_resize(image):
+    img = image
+    height, width = img.shape[:2]
+    
+    img = cv2.resize(img, (450, int(450 * height / width)))
+    
     return img
 
 
