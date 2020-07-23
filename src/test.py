db9098d7094018730fad35da8eabbdac348365e2
remi0628 <safaia0628@yahoo.co.jp>
2020-07-23 16:01:48 +0900

    no message

--- src/test.py
+++ src/test.py
@@ -2,6 +2,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.preprocessing import image
+from overlay import overlayImage
+
 
 def load_image(image_path, grayscale=False, target_size=None):
     pil_image = image.load_img(image_path, grayscale, target_size)
@@ -18,23 +20,52 @@ def draw_bounding_box(face_coordinates, image_array, color):
     x, y, w, h = face_coordinates
     cv2.rectangle(image_array, (x, y), (x + w, y + h), color, 2)
 
-def draw_bounding_box2(face_coordinates, image_array, color, img, emotion_text):
+
+def draw_bounding_box2(face_coordinates, image_array, color, img, imgr):
     x, y, w, h = face_coordinates
     ny = y - 50
     cv2.rectangle(image_array, (x, ny), (x + w, y + h), color, 2)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+    
+    img_face = cv2.resize(img, (int(w/w), int(h/h)))  #変更＊　数を一致させる
+    img2 = image_array.copy()
+    img2[ny:ny+int(h/h), x:x+int(w/w)] = img_face       #変更＊　数を一致させる
+    return img2
+
+
+def draw_bounding_box3(face_coordinates, image_array, color, img, imgr, frame):
+    x, y, w, h = face_coordinates
+    ny = y - 50
+    # cv2.rectangle(image_array, (x, ny), (x + w, y + h), color, 2)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+        
+    img_face = cv2.resize(img, (int(w/5), int(h/5) + 50))  #変更＊　数を一致させる
+    img2 = image_array.copy()
+    img2[ny:ny+int(h/5)+50, x:x+int(w/5)] = img_face       #変更＊　数を一致させる
+    
+    img3 = overlayImage(frame, img2, (0, 0))
+    return img3
+
+
+
+"""
+def draw_bounding_box2(face_coordinates, image_array, color, img, emotion_text):
+    x, y, w, h = face_coordinates
+    
+    cv2.rectangle(image_array, (x, y), (x + w, y + h), color, 2)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-    if emotion_text == 'angry' or emotion_text == 'happy':
-        img_face = cv2.resize(img, (w, int(h/5) + 50))  #変更＊　数を一致させる
-        img2 = image_array.copy()
-        img2[ny:ny+int(h/5)+50, x:x+w] = img_face       #変更＊　数を一致させる
-        return img2
+    if emotion_text == 'happy':
+        img3 =cv2.imread("../../../emotion_gazou/mark_manpu03_ikari.png", -1)
+        img_face = cv2.resize(img, (w, h))
+        img3[y-100:y-100+h, x-100:x-100+w] = img_face
+        return img3
     else:
-        img_face = cv2.resize(img, (w, h + 50))
+        img_face = cv2.resize(img, (w, h))
         img2 = image_array.copy()
-        img2[ny:ny+h+50, x:x+w] = img_face
+        img2[y:y+h, x:x+w] = img_face
         return img2
-
+"""
 
 def apply_offsets(face_coordinates, offsets):
     x, y, width, height = face_coordinates
